f71cd82ece17679c69a9c24f696e971faa8ec3ae
JoelSancho <sancho_joel@yahoo.com>
2020-08-16 21:38:09 -0400

    first commit

---
+++ myscript.js
@@ -0,0 +1,20 @@
+/* Two function that display text message when html button is click */
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "I'm right";
+}
+
+
+function myNewFunction() {
+  document.getElementById("demo2").innerHTML = "No I'm right";
+}
+
+
+
+
+
+
+
+
+
+
---
+++ myscript2.js
@@ -0,0 +1,17 @@
+/*
+function changeBackground() {
+  let text = document.getElementById("hover").alert("No");  
+
+} */
+
+
+
+function displayText() {
+  alert('Hey, I told you not to hover over me!');
+}
+
+
+
+
+
+ 
